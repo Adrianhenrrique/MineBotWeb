74a16ba0a13965bec829f6a565fddd0b2d9387ab
Adrian Henrique <113368090+Adrianhenrrique@users.noreply.github.com>
2025-04-05 11:29:57 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h1>Bem-vindo ao sistema de tickets</h1>
+        <p><a href="/tickets">Ver transcrições</a></p>
+      </div>
+    );
+  }
+  
